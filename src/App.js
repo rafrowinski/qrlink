62317d92dd79e018ae6cd678209d6036d5ae00ca
rafrowinski <rrowinskipl@gmail.com>
2019-01-08 20:59:23 +0100

    adding decryption

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import data from './data'
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+const CryptoJS = require("crypto-js");
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Route path="/:id/:key" component={Decrypt} />
+      </Router>
     );
   }
+}
+
+function Decrypt({ match }) {
+  let text = data[match.params.id];
+
+  var bytes = CryptoJS.AES.decrypt(text, match.params.key);
+  var decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+  return (
+    <div>
+      <h3>{decryptedData}</h3>
+    </div>
+  );
 }
 
 export default App;
